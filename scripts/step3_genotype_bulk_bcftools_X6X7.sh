14d98916dcae63c0c8de4a72417f3367d72c4554
Lukas Weber <lukas.weber.edu@gmail.com>
2021-01-29 21:52:41 -0500

    VCF files from all combinations of 2 samples

---
+++ scripts/step3_genotype_bulk_bcftools_X6X7.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -l mem_free=2G,h_vmem=3G,h_fsize=100G
+
+# qsub <scripts/filename.sh>
+
+######################################################################
+# Shell script to genotype bulk RNA-seq samples using bcftools mpileup
+######################################################################
+
+# This script runs bcftools mpileup to generate a genotype reference VCF file 
+# from the aligned bulk RNA-seq samples. Note that we only use the bulk samples 
+# from the individuals included in the single-cell pool (X4-X7).
+
+# note: requires BAM files from previous scripts "align_index_bulk.sh"
+
+# runtime: ~1 day
+
+
+# --------------------
+# Run bcftools mpileup
+# --------------------
+
+mkdir -p outputs/genotype/bcftools
+
+bcftools mpileup -Ou \
+-f ../../cellranger/refdata-gex-GRCh38-2020-A/fasta/genome.fa \
+outputs/genotype/18431X6/STAR/Aligned.sortedByCoord.out.bam \
+outputs/genotype/18431X7/STAR/Aligned.sortedByCoord.out.bam | \
+bcftools call -mv -Ov \
+-o outputs/genotype/bcftools/bcftools_HGSOC_Dec2020_X6X7.vcf
+
