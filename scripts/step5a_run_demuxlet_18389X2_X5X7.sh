398d09c710f2eef69d5e94a0c62c61ec1a0248e8
Lukas Weber <lukas.weber.edu@gmail.com>
2021-01-31 01:07:35 -0500

    run demuxlet for 2 samples genotype X5X7

---
+++ scripts/step5a_run_demuxlet_18389X2_X5X7.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -l mem_free=5G,h_vmem=10G,h_fsize=100G
+
+# qsub <scripts/filename.sh>
+
+##############################
+# Shell script to run demuxlet
+##############################
+
+# run demuxlet to demultiplex samples
+
+# requires genotype reference from bcftools
+
+# runtime:
+
+
+mkdir -p outputs/demuxlet/18389X2_X5X7
+
+demuxlet \
+--sam outputs/cellranger/18389X2/outs/possorted_genome_bam.bam \
+--group-list outputs/cellranger/18389X2/outs/filtered_feature_bc_matrix/barcodes.tsv.gz \
+--alpha 0 --alpha 0.5 \
+--vcf outputs/genotype/bcftools/bcftools_HGSOC_Dec2020_X5X7.vcf \
+--field GT \
+--out outputs/demuxlet/18389X2_X5X7
+
