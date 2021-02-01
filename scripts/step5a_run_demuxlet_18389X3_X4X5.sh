26cf05102fdc00c1fb416411dca952c3266f73d4
Lukas Weber <lukas.weber.edu@gmail.com>
2021-01-31 22:45:59 -0500

    additional demuxlet runs

---
+++ scripts/step5a_run_demuxlet_18389X3_X4X5.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -l mem_free=5G,h_vmem=10G,h_fsize=100G
+
+# qsub <scripts/filename.sh>
+
+##############################
+# Shell script to run demuxlet
+##############################
+
+# run demuxlet to demultiplex samples
+
+# requires genotype reference from bcftools
+
+# runtime:
+
+
+mkdir -p outputs/demuxlet/18389X3_X4X5
+
+demuxlet \
+--sam outputs/cellranger/18389X3/outs/possorted_genome_bam.bam \
+--group-list outputs/cellranger/18389X3/outs/filtered_feature_bc_matrix/barcodes.tsv.gz \
+--alpha 0 --alpha 0.5 \
+--vcf outputs/genotype/bcftools/bcftools_HGSOC_Dec2020_X4X5.vcf \
+--field GT \
+--out outputs/demuxlet/18389X3_X4X5/18389X3_X4X5
+
